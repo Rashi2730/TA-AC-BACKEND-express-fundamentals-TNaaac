016bd1f5e0e1af3492439ed70e230f23128cdc93
Rashi Israni <rashiisrani97@gmail.com>
2022-12-16 16:45:10 +0530

    ishh

--- block-BNaabq/server.js
+++ block-BNaabq/server.js
@@ -1,27 +1,25 @@
 // Create a basic express server
 var express = require('express');
-
+var logger = require('morgan');
+var cookieParser = require('cookieparser');
 var app = express();
 
-var logger = require('morgan');
-
-var cookieParser = require('cookieparser');
-
-app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 
 app.use(cookieParser());
+app.use(logger('dev'));
 
-app.use('/about', (res, req, next) => {
+app.use((req, res, next) => {
   console.log(req.cookies);
-  res.cookie('username', 'suraj');
   next();
 });
 
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(__dirname + '/public'));
+app.use('/about', (req, res, next) => {
+  res.cookie('username', 'suraj');
+  res.end('About Page');
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
